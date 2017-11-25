69090b40d47dad75904be4a9f8d37f98a772bebc
unknown <matengfeichn@163.com>
2017-11-25 13:21:12 +0800

    add axios

--- onecardweb/src/components/Home.js
+++ onecardweb/src/components/Home.js
@@ -2,12 +2,14 @@
 import React, { Component } from 'react';
 import '../css/public.css'
 import {connect} from 'react-redux'
+import axios from 'axios'
 
 const HomeUI = (props)=>{
 	//页面内容
 	return(
 		<div className="Home">
 			<input type="text" id="fid"/>
+			{props.getData()}
 		    <button onClick={()=>{props.addToDo(document.getElementById("fid").value)}}>add todo</button>
 		    <ul>
 		    	{
@@ -35,8 +37,23 @@ const mapDispatchToProps = (dispatch)=>{
 			dispatch({
 				type:"ADD_TODO",
 				payload: data
+			})			
+		},
+		getData:function(){
+			var that = this
+			axios.get('/users/getdata')
+			.then(function(res){
+				console.log(res)
+//				var data = res.data.data.films;
+//				console.log(data)
+//				console.log(that.props)
+//				console.log(that.props.store)
+	//			that.addState(data)
+//				that.props.store.dispatch({
+//						type:"FILMS",
+//						payload:data
+//				})
 			})
-			
 		}
 	}
 }
